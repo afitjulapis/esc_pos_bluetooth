a67f60063fd4dee3665b050a4f968e8ea5ffb918
Afiq Zulhafiz <53424005+afitjulapis@users.noreply.github.com>
2021-05-17 12:28:40 +0800

    Update printer_bluetooth_manager.dart

--- lib/src/printer_bluetooth_manager.dart
+++ lib/src/printer_bluetooth_manager.dart
@@ -138,12 +138,12 @@ class PrinterBluetoothManager {
     });
 
     // Printing timeout
-//     _runDelayed(timeout).then((dynamic v) async {
-//       if (_isPrinting) {
-//         _isPrinting = false;
-//         completer.complete(PosPrintResult.timeout);
-//       }
-//     });
+    _runDelayed(timeout).then((dynamic v) async {
+      if (_isPrinting) {
+        _isPrinting = false;
+        completer.complete(PosPrintResult.success);
+      }
+    });
 
     return completer.future;
   }
